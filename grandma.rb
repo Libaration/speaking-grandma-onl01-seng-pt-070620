b6f475b9a31a92de3cc3859e0b3be96ad239e620
Cristian Deleon <libaration@gmail.com>
2020-07-06 22:30:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-    if phrase == phrase.upcase
+    if phrase.upcase
        return "NO, NOT SINCE 1938!"
     else
 return "HUH?! SPEAK UP, SONNY!"
